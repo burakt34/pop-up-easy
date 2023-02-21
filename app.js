7060befe41623c6de629172aa872f906996c7ead
Burak Tanyel <113188482+burakt34@users.noreply.github.com>
2023-02-21 18:15:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const sil = document.getElementById("dlt1");
+const alll = document.querySelector(".container");
+const button = document.querySelector(".btn")
+
+sil.addEventListener("click", function(){
+    alll.remove();
+});
